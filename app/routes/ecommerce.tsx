bbe5d9c02034a22896e1eb7ce0c30f8f39bbeb12
Brooks Lybrand <lybrand.brooks@heb.com>
2022-05-09 07:54:59 -0500

    Add back product-quickviews (now styles are fixed)

--- app/routes/ecommerce.tsx
+++ app/routes/ecommerce.tsx
@@ -4,7 +4,12 @@ import Sidebar from '~/components/sidebar';
 export default function Marketing() {
   return (
     <Sidebar
-      subPages={['category-previews', 'product-features', 'product-lists']}
+      subPages={[
+        'category-previews',
+        'product-features',
+        'product-lists',
+        'product-quickviews',
+      ]}
     >
       <Outlet />
     </Sidebar>
